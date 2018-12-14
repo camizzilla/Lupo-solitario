35ce29832b6db3be00f6bb2488e5da7597c2b377
Roberto <camizzilla@hotmail.com>
2018-12-14 13:51:06 +0100

    Creazione della classe game

--- js/main.js
+++ js/main.js
@@ -1,9 +1,41 @@
 class game  {
   constructor(){
+    this.livelloFacile = false;
     this.artiRamas = [
       "Mimetismo",
-      "Scherma"
+      "Caccia",
+      "Sesto senso",
+      "Orientamento",
+      "Scherma",
+      "Psicoschermo",
+      "Psicolaser",
+      "Affinità animale",
+      "Telecinesi"
       ];
+    this.armi = [
+      "Pugnale",
+      "Lancia",
+      "Mazza",
+      "Daga",
+      "Martello",
+      "Spada",
+      "Ascia",
+      "Spada",
+      "Asta",
+      "Spadone"
+      ];
+      this.oggettiTrovati = [
+        "Spada",
+        "Elmo",
+        "Pasti",
+        "Cotta di maglia",
+        "Mazza",
+        "Pozione magica",
+        "Asta",
+        "Lancia",
+        "Corone d'oro",
+        "Spadone"
+        ];
   }
 }
 
@@ -11,6 +43,14 @@ class player {
   constructor(comb, res) {
     this.combattivita = 20 + comb;
     this.resistenza = 10 + res;
+    this.artiRamas = [];
+    this.zaino = {
+      pasti: 1
+    };
+    this.armi = [];
+    this.borsa = {
+      coroneOro : 0
+    }
   }
  
  
